37a23573eceeb33db18959281fdfc99541b7cabc
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-06-12 21:57:18 +0000

    Added a basic shell...

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ system ? builtins.currentSystem }:
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { inherit system; config = {}; overlays = []; };
+in pkgs.mkShellNoCC {
+  NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+  
+  shellHook = ''
+    git config user.name "Adam Arbour"
+    git config user.email "845679+adamarbour@users.noreply.github.com"
+    git config init.defaultBranch main
+  '';
+  
+  packages = with pkgs; [
+    age
+    git
+    just
+    npins
+    sops
+    ssh-to-age
+  ];
+}
